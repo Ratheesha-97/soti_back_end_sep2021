9f241506940fb94e81556081023d0dd1a32bccf3
Ratheesha-97 <ratheesha1997@gmail.com>
2021-09-14 17:08:48 +0530

    day4

---
+++ HospitalMngmt/HospitalMngmt/IndexedClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMngmt
+{
+    class IndexedClass
+    {
+        private String[] names = new string[10];
+        public String this[int index]
+        {
+            get
+            {
+                return (index >= 0 && index <= 9) ? names[index] : "";
+            }
+            set
+            {
+                if (index >= 0 && index <= 9)
+                {
+                    names[index] = value;
+                }
+            }
+        }
+
+        public int this[string name]
+        {
+            get
+            {
+                int index = 0;
+                while (index < 10)
+                {
+                    if(names[index] == name)
+                    {
+                        return index;
+                    }
+                }
+            }
+            set
+            {
+                if (index >= 0 && index <= 9)
+                {
+                    names[index] = value;
+                }
+            }
+        }
+    }
+}
---
+++ HospitalMngmt/HospitalMngmt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMngmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndexedClass nameList = new IndexedClass();
+            nameList[0] = "Ram";
+            Console.WriteLine(nameList[0]);
+            nameList[1] = "abc";
+            Console.WriteLine(nameList[1]);
+
+        }
+    }
+    
+}
